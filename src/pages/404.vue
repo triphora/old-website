eabde5af7d5e93c5695dcb28df87e21e8bf6bbe0
Emma Waffle <emma@waffle.coffee>
2021-05-09 01:25:18 -0400

    Single subdomain!

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <div class="pt-8 md:pt-16">
-      <h1 class="text-center font-bold text-4xl text-ui-primary">Oh no! There is nothing here.</h1>
+      <h1 class="text-center font-bold text-4xl text-ui-primary">404 - Whoops... there's nothing here.</h1>
     </div>
     <div class="mb-10 flex items-center justify-center">
       <g-link to="/" class="font-bold border-b border-ui-primary text-xl">Go home</g-link>.
