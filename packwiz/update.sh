4fd1d17853257aa816d2c5e229c27de55e8993eb
Emma Waffle <emmaffle@modrinth.com>
2021-12-30 01:30:04 -0500

    update mods, update my-work

--- packwiz/update.sh
+++ packwiz/update.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 micro pack.toml
 packwiz update -a
-packwiz refresh
-cd modrinth/
-./packwiz2mr.py -i .. -o .
-unzip -qof waffle\'s\ Modpack.zip index.json
-rm waffle\'s\ Modpack.zip
-zip -q waffle\'s\ Modpack.zip -r overrides/ index.json
-cd ..
+rm waffle\'s\ Modpack-*.zip
+packwiz modrinth export
 packwiz refresh
 git add :
 git commit -a
