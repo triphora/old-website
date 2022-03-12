7b21ad5f3373c81f02b256073351bc6a08960e45
Emma Waffle <emmaffle@modrinth.com>
2022-03-12 14:26:28 -0500

    readd bobby, finalize 1.18.2 changes

--- packwiz/mrupload.sh
+++ packwiz/mrupload.sh
@@ -8,30 +8,21 @@ fi
 REQUIRED_MOD_FILES=$(grep -L "\\[option\\]" mods/*.toml | tr -s '\n' ' ')
 OPTIONAL_MOD_FILES=$(grep -l "\\[option\\]" mods/*.toml | tr -s '\n' ' ')
 
-REQUIRED_PROJECT_IDS=($(grep -h "mod-id =.*" $REQUIRED_MOD_FILES | sed "s/mod-id = //;s/\"//g" | tr -s '\n' ' '))
-REQUIRED_VERSION_IDS=($(grep -h "version =.*" $REQUIRED_MOD_FILES | sed "s/version = //;s/\"//g" | tr -s '\n' ' '))
-
 OPTIONAL_PROJECT_IDS=($(grep -h "mod-id =.*" $OPTIONAL_MOD_FILES | sed "s/mod-id = //;s/\"//g" | tr -s '\n' ' '))
 OPTIONAL_VERSION_IDS=($(grep -h "version =.*" $OPTIONAL_MOD_FILES | sed "s/version = //;s/\"//g" | tr -s '\n' ' '))
 
 DEPENDENCIES=""
-for PROJECT in ${!REQUIRED_PROJECT_IDS[@]}; do
-  for VERSION in ${!REQUIRED_VERSION_IDS[@]}; do
-    [[ $PROJECT != $VERSION ]] && continue
-    DEPENDENCIES+="{\"project_id\":\"${REQUIRED_PROJECT_IDS[$PROJECT]}\",\"version_id\":\"${REQUIRED_VERSION_IDS[$VERSION]}\",\"dependency_type\":\"required\"},"
-  done
+for VERSION in ${!REQUIRED_VERSION_IDS[@]}; do
+  DEPENDENCIES+="{\"version_id\":\"${REQUIRED_VERSION_IDS[$VERSION]}\",\"dependency_type\":\"required\"},"
 done
-for PROJECT in ${!OPTIONAL_PROJECT_IDS[@]}; do
-  for VERSION in ${!OPTIONAL_VERSION_IDS[@]}; do
-    [[ $PROJECT != $VERSION ]] && continue
-    DEPENDENCIES+="{\"project_id\":\"${OPTIONAL_PROJECT_IDS[$PROJECT]}\",\"version_id\":\"${OPTIONAL_VERSION_IDS[$VERSION]}\",\"dependency_type\":\"optional\"},"
-  done
+for VERSION in ${!OPTIONAL_VERSION_IDS[@]}; do
+  DEPENDENCIES+="{\"version_id\":\"${OPTIONAL_VERSION_IDS[$VERSION]}\",\"dependency_type\":\"optional\"},"
 done
 DEPENDENCIES=${DEPENDENCIES%,}
 
 curl -isX POST \
   -H "Authorization:$MODRINTH_TOKEN" -H "Content-Type:multipart/form-data" -H "Accept:application/json" \
-  -F "data={\"file_parts\":[\"file\"], \"version_number\":\"$1\", \"version_title\":\"$1\", \"version_body\":\"$2\", \"loaders\":[\"fabric\"], \"game_versions\":[\"1.18.1\"], \"version_type\":\"release\", \"release_channel\":\"release\", \"project_id\":\"sISTMo6m\", \"featured\":false, \"dependencies\":[$DEPENDENCIES]}" \
+  -F "data={\"file_parts\":[\"file\"], \"version_number\":\"$1\", \"version_title\":\"$1\", \"version_body\":\"$2\", \"loaders\":[\"fabric\"], \"game_versions\":[\"1.18.2\"], \"version_type\":\"release\", \"release_channel\":\"release\", \"project_id\":\"sISTMo6m\", \"featured\":false, \"dependencies\":[$DEPENDENCIES]}" \
   -F "file=@waffle's Modpack-$1.mrpack" https://api.modrinth.com/v2/version
 
 echo ""
