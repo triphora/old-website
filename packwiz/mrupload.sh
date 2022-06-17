4213ce9a51a440a1c0a69776a634dc7bbcb1d7ed
Emma Cypress Pointer-Null <emmaffle@modrinth.com>
2022-06-17 15:48:51 -0400

    Update bash scripts

--- packwiz/mrupload.sh
+++ packwiz/mrupload.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
-if [ -z "$1" ] || [ -z "$2" ] || [ -n "$3" ]; then
-  echo "version number or changelog not supplied, or third arg supplied"
-  echo "example use: ./mrupload 1940.10.28 \"- Said όχι\""
-  exit
+VERSION=$DATE
+
+if [ -z "$VERSION" ]; then
+  read -ep "Enter version: " VERSION
 fi
+
+read -ep "Enter changelog: " CHANGELOG
 
 curl -isX POST \
   -H "Authorization:$MODRINTH_TOKEN" -H "Content-Type:multipart/form-data" -H "Accept:application/json" \
-  -F "data={\"file_parts\":[\"file\"], \"version_number\":\"$1\", \"version_title\":\"$1\", \"version_body\":\"$2\", \"loaders\":[\"quilt\"], \"game_versions\":[\"1.18.2\"], \"version_type\":\"release\", \"project_id\":\"sISTMo6m\", \"featured\":false, \"dependencies\":[]}" \
-  -F "file=@waffle's Modpack-$1.mrpack" https://api.modrinth.com/v2/version
+  -F "data={\"file_parts\":[\"file\"], \"version_number\":\"$VERSION\", \"version_title\":\"$VERSION\", \"version_body\":\"$CHANGELOG\", \"loaders\":[\"quilt\"], \"game_versions\":[\"1.18.2\"], \"version_type\":\"release\", \"project_id\":\"sISTMo6m\", \"featured\":false, \"dependencies\":[]}" \
+  -F "file=@waffle's Modpack-$VERSION.mrpack" https://api.modrinth.com/v2/version
 
-echo ""
+echo
--- packwiz/update.sh
+++ packwiz/update.sh
@@ -1,8 +1,39 @@
 #!/usr/bin/env bash
-micro pack.toml
-packwiz update -a
-rm *.mrpack
-packwiz modrinth export
-packwiz refresh
+
+# https://stackoverflow.com/a/29436423
+# https://stackoverflow.com/a/29512084
+RESET="\033[0m"
+RED="\033[31;1m"
+GREEN="\033[32;1m"
+function yes_or_no {
+  while true; do
+    read -p "$(echo -e $GREEN$1?$RESET) [y/n]: " yn
+    case $yn in
+      [Yy]*) return 0  ;;
+      [Nn]*) echo -e $RED"Skipping "$2$RESET ; return  1 ;;
+    esac
+  done
+}
+
+printf -v DATE '%(%Y.%m.%d)T'
+
+sed -i "s/version = \".*\..*\..*\"/version = \"$DATE\"/" pack.toml
+
+if yes_or_no "Perform pack update" "update"; then
+  packwiz update -a
+fi
+echo
+
 git add :
-git commit -a
+
+if yes_or_no "Perform commit" "commit"; then
+  git commit -am "Update mods $DATE"
+fi
+echo
+
+if yes_or_no "Push and upload to Modrinth" "upload"; then
+  rm *.mrpack
+  packwiz modrinth export
+  ./mrupload.sh $DATE
+  git push
+fi
