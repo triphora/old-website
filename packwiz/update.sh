2547e6f7fb51a81b8db3efdac9761aacb673dfd8
Emma Cypress Pointer-Null <emmaffle@modrinth.com>
2022-06-26 14:25:51 -0400

    Update to 1.19

--- packwiz/update.sh
+++ packwiz/update.sh
@@ -37,8 +37,7 @@ if yes_or_no "Push and upload to Modrinth" "upload"; then
   read -ep "Enter changelog: " CHANGELOG
   curl -isX POST \
     -H "Authorization:$MODRINTH_TOKEN" -H "Content-Type:multipart/form-data" -H "Accept:application/json" \
-    -F "data={\"file_parts\":[\"file\"], \"version_number\":\"$VERSION\", \"version_title\":\"$VERSION\", \"version_body\":\"$CHANGELOG\", \"loaders\":[\"quilt\"], \"game_versions\":[\"1.18.2\"], \"version_type\":\"release\", \"project_id\":\"sISTMo6m\", \"featured\":false, \"dependencies\":[]}" \
+    -F "data={\"file_parts\":[\"file\"], \"version_number\":\"$DATE\", \"version_title\":\"$DATE\", \"version_body\":\"$CHANGELOG\", \"loaders\":[\"quilt\"], \"game_versions\":[\"1.19\"], \"version_type\":\"release\", \"project_id\":\"sISTMo6m\", \"featured\":false, \"dependencies\":[]}" \
     -F "file=@waffle's Modpack-$VERSION.mrpack" https://api.modrinth.com/v2/version
-  echo
   git push
 fi
